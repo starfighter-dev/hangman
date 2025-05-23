0a79b73c296a7f5ceea3f76875a99bd8429aed3e
Martin Elliott <martin@chirohosting.com>
2025-05-23 10:48:29 +0100

    Work in progress. Needs to store state.

--- hangman-web.php
+++ hangman-web.php
@@ -1,5 +1,38 @@
-<html>
+<?php
+require_once 'Hangman.php';
+
+$game = new Hangman();
+
+
+?><!DOCTYPE html>
+<html lang="en">
    <head>
+      <meta charset="UTF-8">
       <title>Hangman</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+      <style type="text/css">
+         .wordProgress {
+            font-size: 2em;
+            letter-spacing: 0.2em;
+         }
+      </style>
    </head>
+   <body>
+      <h1>Hangman</h1>
+      <div class="wordProgress"><?= $game->getWordProgress() ?></div>
+      <div class="guessedLetters"><?= implode(', ', $game->getGuessedLetters()) ?></div>
+      <?php if ( !$game->isWon() && !$game->isLost() ): ?>
+         <form method="post">
+            <label for="letter">Guessed a letter:</label>
+            <input type="text" name="letter" maxlength="1" required autofocus />
+            <button type="submit" class="btn btn-primary">Guess</button>
+         </form>
+      <?php endif; ?>
+      <?php if ($game->isWon()): ?>
+         <div class="message">Congratulations! You guessed the word: <strong><?= $game->getWord() ?></strong></div>
+       <?php elseif ($game->isLost()): ?>
+         <div class="message wrong">Game over! The word was: <strong><?= $game->getWord() ?></strong></div>
+      <?php endif; ?>
+
+   </body>
 </html>
